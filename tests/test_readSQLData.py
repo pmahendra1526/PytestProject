2e17501c49be1730ab4231ff87e54c27797f8bbb
unknown <pmahendra1526@gmail.com>
2025-12-29 23:25:30 +0530

    additional changes

--- tests/test_readSQLData.py
+++ tests/test_readSQLData.py
@@ -1,8 +1,14 @@
 import pytest
+import pandas as pd
 from src.dbConnection import create_connection
-
 
 driver_name="{ODBC Driver 17 for SQL Server}"
 server_name="DESKTOP-59J1BAM\\SQLEXPRESS" # r"DESKTOP-59J1BAM\SQLEXPRESS"
 database_name="Mahendra"
-create_connection(driver_name,server_name, database_name)
+conn=create_connection(driver_name,server_name, database_name)
+
+sql_query = "SELECT * FROM EMP"
+df=pd.read_sql_query(sql_query,conn)
+print(df)
+df1=pd.read_sql_query("SELECT * FROM EMPLOYEES",conn)
+print(df1)
